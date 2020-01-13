202f8cd7831d8dc7953c14dbefc85e57bac7eb37
Jonathan Dain Palacio <bangarangler@gmail.com>
2020-01-13 07:30:43 -0500

    Added js to toggle mobile nav

--- shared.js
+++ shared.js
@@ -2,22 +2,43 @@ const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const modalNoButton = document.querySelector(".modal__action--negative");
 const selectPlanButtons = document.querySelectorAll(".plan button");
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", () => {
-    modal.style.display = "block";
-    backdrop.style.display = "block";
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    modal.classList.add('open')
+    backdrop.classList.add('open')
   })
 }
 
 const closeModal = () => {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
+  // backdrop.style.display = "none";
+  // modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove('open')
+  }
+  backdrop.classList.remove('open')
 }
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", () => {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
+  closeModal();
+});
 
-modalNoButton.addEventListener("click", closeModal);
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+toggleButton.addEventListener('click', function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add('open')
+  backdrop.classList.add('open')
+})
 
 
 
